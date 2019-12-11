881efa2f3f8b869baaf3e8caec5fd076a254baaa
Loghan Ramassamy <loghan.r@gmail.com>
2019-12-11 16:46:46 +0100

    Grid size: make it explicit

--- src/main/java/fr/lramss/MarsRover.java
+++ src/main/java/fr/lramss/MarsRover.java
@@ -1,11 +1,12 @@
 package fr.lramss;
 
 public class MarsRover {
+    private static final int GRID_SIZE = 10;
     int x = 0;
     int y = 0;
 
     public String execute(String commands) {
-        int nbMoves = commands.length()%10;
+        int nbMoves = commands.length() % GRID_SIZE;
         return x + ":" + moveY(nbMoves) + ":N";
     }
 
